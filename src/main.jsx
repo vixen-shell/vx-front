faafaa57d09c91ab6bcc9cc85dcf4223834b97f6
Nohavye <noha.poncelet@gmail.com>
2024-09-04 17:47:30 +0200

    chore: TS -> JS

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { Root } from 'vx-front'
+
+Root.create(document.getElementById('root')).render((feature) =>
+    import(`./features/${feature}/index.jsx`)
+)
--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { Root } from 'vx-front'
-
-Root.create(document.getElementById('root')!).render(
-    (feature) => import(`./features/${feature}/index.tsx`)
-)
