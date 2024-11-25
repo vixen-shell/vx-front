170425f0690a161cc2c5dbca77eca1629388cd87
Nohavye <noha.poncelet@gmail.com>
2024-11-25 14:50:25 +0100

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        fs: {
+            strict: false,
+        },
+    },
     resolve: {
         alias: {
             '@vx-components': 'vx-front/dist/interface/components',
